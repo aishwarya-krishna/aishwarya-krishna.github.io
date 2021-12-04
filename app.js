eb70d4457488a443a1faba018e64274b8fa485ef
aishwarya-krishna <37927999+aishwarya-krishna@users.noreply.github.com>
2021-12-04 21:56:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ $span.style.cssText = `
     font-size: ${parseInt(css.fontSize)}px;
     padding-left: ${parseInt(css.paddingLeft)}px;
     padding-top: ${parseInt(css.paddingTop) + 1}px;
-    margin-top: ${parseInt(css.marginTop)}px;
-    margin-left: ${parseInt(css.marginLeft)}px;
+    /* margin-top: ${parseInt(css.marginTop)}px;
+    margin-left: ${parseInt(css.marginLeft)}px; */
 `;
 
 document.body.appendChild($span);
